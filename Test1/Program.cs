0207476f13538fbc6e7b5cc071b496c6701d0e85
unknown <Student@SHJ-LNR-PC1.esri.com>
2017-07-05 14:36:36 +0400

    Rest of project

---
+++ Test1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void foo(int x)
+        {
+            Console.WriteLine(x.ToString() + " is why I live");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yodeleying Yak!");
+            foo(37);
+        }
+    }
+}
